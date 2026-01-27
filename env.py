87dba7230e402c397b0df222ea078926e6ac5a65
Gigabite1277 <neiltraining2024@gmail.com>
2026-01-27 17:34:38 +0000

    Testing out .gitignore re PostgreSQL URL

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "")
+    "DATABASE_URL", "postgresql://neondb_owner:npg_kPoyKBpV8Hi1@ep-royal-dew-agmrmk27.c-2.eu-central-1.aws.neon.tech/boat_blimp_pasta_580530")
