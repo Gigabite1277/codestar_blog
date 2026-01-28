7c289885ca636ac3dd851fb102a6471ae4c7be2a
Gigabite1277 <neiltraining2024@gmail.com>
2026-01-28 12:45:44 +0000

    Made chages to secret key etc

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_kPoyKBpV8Hi1@ep-royal-dew-agmrmk27.c-2.eu-central-1.aws.neon.tech/boat_blimp_pasta_580530")
+os.environ.setdefault("django-insecure-0$0wkuy4&n5ibz+1x+d8)9i_ofxwlnb0@h&t9^nlxo6lfb)e9u", "<Foxtrot1277>")
