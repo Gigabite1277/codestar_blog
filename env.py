3bbce4205034b5feb2b4cca7325b8726544e4902
Gigabite1277 <neiltraining2024@gmail.com>
2026-01-30 16:07:48 +0000

    Commit for Heroku test

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_kPoyKBpV8Hi1@ep-royal-dew-agmrmk27.c-2.eu-central-1.aws.neon.tech/boat_blimp_pasta_580530")
-os.environ.setdefault("SECRET_KEY", "Foxtrot1277")
+os.environ.setdefault("SECRET_KEY", "<Foxtrot1277>")
