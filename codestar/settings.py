98a20f73701ed8462bb297aeb58c5872afd9315b
Gigabite1277 <neiltraining2024@gmail.com>
2026-02-12 22:22:16 +0000

    General Commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '.localhost:8000']
+ALLOWED_HOSTS = ['.herokuapp.com', '.localhost']
 
 
 # Application definition
