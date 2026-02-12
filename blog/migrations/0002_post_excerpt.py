94ef874b73ce38e36038f630f8e30ce3f835a37d
Gigabite1277 <neiltraining2024@gmail.com>
2026-02-12 23:02:12 +0000

    Practiced adding fields to a dadabase using

--- blog/migrations/0002_post_excerpt.py
+++ blog/migrations/0002_post_excerpt.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("blog", "0002_post_excerpt"),
+        ("blog", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name="post",
-            name="updated_on",
-            field=models.DateTimeField(auto_now=True),
+            model_name='post',
+            name='excerpt',
+            field=models.TextField(blank=True),
         ),
     ]
