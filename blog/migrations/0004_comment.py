c36e8744b873f1079813bca2f9c8995e7b0a1d44
Gigabite1277 <neiltraining2024@gmail.com>
2026-04-17 18:43:26 +0000

    Heroku Commit

--- blog/migrations/0004_comment.py
+++ blog/migrations/0004_comment.py
@@ -24,4 +24,3 @@ class Migration(migrations.Migration):
                 ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='blog.post')),
             ],
         ),
-    ]
