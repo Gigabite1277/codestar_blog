8fa6b255c826d853d479c9a470bcf0724d888632
Gigabite1277 <neiltraining2024@gmail.com>
2026-04-19 19:05:37 +0000

    Heroku commit

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_kPoyKBpV8Hi1@ep-royal-dew-agmrmk27.c-2.eu-central-1.aws.neon.tech/boat_blimp_pasta_580530")
 os.environ.setdefault("SECRET_KEY", "<Foxtrot1277>")
+
+os.environ.setdefault("CLOUDINARY_URL","cloudinary://478459519146958:Im9EiiNqrbulaQRG2PSKZnL9yAw@dgdryqwyp")
